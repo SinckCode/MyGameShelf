4fcaecdcf85ec09e52dbb23c12d4e2e8377e070e
SinckCode <soyangeldavid1@gmail.com>
2025-11-02 16:39:04 -0600

    Subiendo Tipografia

--- app/src/main/java/com/example/mygameshelf/ui/theme/Theme.kt
+++ app/src/main/java/com/example/mygameshelf/ui/theme/Theme.kt
@@ -52,7 +52,7 @@ fun MyGameShelfTheme(
 
     MaterialTheme(
         colorScheme = colorScheme,
-        typography = Typography,
+        typography = poppinsTypo,
         content = content
     )
 }
